22a6ad9d77ed8245f0b1f86d38dd0c6031fe37fb
jrdrwn <jordirwn@gmail.com>
2021-02-15 08:47:49 +0700

    with-oop

--- main.py
+++ main.py
@@ -3,14 +3,85 @@ from os import system
 from youtube_dl import YoutubeDL, utils
 
 
-RMV = "\033[A\033[2K"
+class YoutubeDownloader:
+    def __init__(self, url=None):
+        self.url = url
 
+    def download(self, url):
+        f = self.get_format(url)
+        a = list(map(lambda x: int(x.split()[0]), f.splitlines()[1:]))
 
-class MyLogger:
+        print(f)
+
+        while True:
+            format_id = int(input("\a\n[format_id] ~> "))
+            if format_id in a:
+                break
+            print("Mohon masukkan format_id dengan benar")
+
+        opt = {
+            "format": f"{format_id}+249/{format_id}+250/{format_id}+251",
+            "progress_hooks": [self.progress_hooks],
+            "logger": self,
+        }
+
+        with YoutubeDL(opt) as ytdl:
+            ytdl.download([url])
+
+    def get_format(self, url):
+        with YoutubeDL({"quiet": True}) as ytdl:
+            meta = ytdl.extract_info(url, download=False)["formats"]
+            afs = meta[0]["filesize"]
+            meta = sorted(meta, key=lambda x: int(x["format_id"]))
+            meta = list(
+                map(
+                    lambda x: [
+                        x["format_id"],
+                        x["format_note"],
+                        self.humanbytes(x["filesize"], afs=afs),
+                    ],
+                    filter(lambda x: "p" in x["format_note"], meta),
+                )
+            )
+            header = ["format_id", "resolusi", "size"]
+            return utils.render_table(header, meta)
+
+    def humanbytes(self, num, afs=0, suffix="B"):
+        units = ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"]
+        try:
+            num += afs
+            for unit in units:
+                if abs(num) < 1024.0:
+                    return "%3.1f%s%s" % (num, unit, suffix)
+                num /= 1024.0
+            return "%.1f%s%s" % (num, "Yi", suffix)
+        except:
+            return "NULL"
+
+    def progress_hooks(self, x):
+        if x["status"] == "downloading":
+            print(
+                f"[download] {x['_percent_str']} {x['_speed_str']}", end="\r"
+            )
+        elif x["status"] == "finished":
+            print("Proses download telah selesai")
+
+    def start(self):
+        if self.url:
+            self.download(self.url)
+        else:
+            while True:
+                url = str(input("\a[url] ~> "))
+                if url:
+                    self.download(url.strip())
+                    break
+                print("Mohon masukkan url youtube!")
+        print("Selesai")
+
     def debug(self, msg):
         if "Merging" in msg:
             print("Sedang menggabungkan video dengan audio")
-        if (
+        elif (
             "f249" in msg or "f250" in msg or "f251" in msg
         ) and "Destination" in msg:
             print("Sedang mendownload audio")
@@ -24,81 +95,4 @@ class MyLogger:
         pass
 
 
-def download(url):
-    f = get_format(url)
-    a = list(map(lambda x: int(x.split()[0]), f.splitlines()[1:]))
-
-    print(f)
-
-    while True:
-        format_id = int(input("\a\n[format_id] ~> "))
-        print(f"{RMV}\r{RMV}", end="\r")
-        if format_id in a:
-            break
-        print("Mohon masukkan format_id dengan benar")
-
-    system("clear")
-
-    opt = {
-        "format": f"{format_id}+249/{format_id}+250/{format_id}+251",
-        "progress_hooks": [progress_hooks],
-        "logger": MyLogger(),
-    }
-
-    with YoutubeDL(opt) as ytdl:
-        ytdl.download([url])
-
-
-def sizeof_fmt(num, afs=0, suffix="B"):
-    units = ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"]
-    try:
-        num += afs
-        for unit in units:
-            if abs(num) < 1024.0:
-                return "%3.1f%s%s" % (num, unit, suffix)
-            num /= 1024.0
-        return "%.1f%s%s" % (num, "Yi", suffix)
-    except:
-        return "NULL"
-
-
-def get_format(url):
-    with YoutubeDL({"quiet": True}) as ytdl:
-        meta = ytdl.extract_info(url, download=False)["formats"]
-        afs = meta[0]["filesize"]
-        meta = sorted(meta, key=lambda x: int(x["format_id"]))
-        meta = list(
-            map(
-                lambda x: [
-                    x["format_id"],
-                    x["format_note"],
-                    sizeof_fmt(x["filesize"], afs=afs),
-                ],
-                filter(lambda x: "p" in x["format_note"], meta),
-            )
-        )
-        header = ["format_id", "resolusi", "size"]
-        return utils.render_table(header, meta)
-
-
-def progress_hooks(x):
-    if x["status"] == "downloading":
-        print(f"[download] {x['_percent_str']} {x['_speed_str']}", end="\r")
-    elif x["status"] == "finished":
-        print("Proses download telah selesai")
-
-
-def start():
-    system("clear")
-    # download(url=URL)
-    while True:
-        url = str(input("\a[url] ~> "))
-        print(f"{RMV}\r{RMV}", end="\r")
-        if url:
-            download(url.strip())
-            print("Selesai")
-            break
-        print("Mohon masukkan url youtube!")
-
-
-start()
+YoutubeDownloader().start()
